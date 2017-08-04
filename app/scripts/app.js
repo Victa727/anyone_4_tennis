c6959b6d26cdf93e25f3d1d2ecc7719f94675dcf
VICTOR AGUILUZ <victa727@yahoo.com>
2017-08-03 17:50:00 -0700

    Capstone reboot

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name anyone4TennisApp
+ * @description
+ * # anyone4TennisApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('anyone4TennisApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name anyone4TennisApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the anyone4TennisApp
+ */
+angular.module('anyone4TennisApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
