f917a15481a179c981add8ab3b17925d6985e44d
VICTOR AGUILUZ <victa727@yahoo.com>
2017-08-06 20:40:04 -0700

    map with current postion

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -9,8 +9,13 @@
  */
 angular.module('anyone4TennisApp')
   .controller('MapCtrl',function(NgMap) {
+    var vm = this;
+    vm.message = 'You can not hide. :)';
     NgMap.getMap().then(function(map) {
-      console.log('markers', map.markers);
-      console.log('shapes', map.shapes);
-    });
+      vm.map = map;
+      });
+      vm.callbackFunc = function(param) {
+      console.log('I know where '+ param +' are. ' + vm.message);
+      console.log('You are at' + vm.map.getCenter());
+    };
 });
